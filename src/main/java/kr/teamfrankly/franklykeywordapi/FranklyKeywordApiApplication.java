3e43e500a4ebc3ae743d4ec1f9a8e9e4d7cf1533
jehyeon <s85737@gmail.com>
2021-11-21 00:30:38 +0900

    spring init

---
+++ src/main/java/kr/teamfrankly/franklykeywordapi/FranklyKeywordApiApplication.java
@@ -0,0 +1,13 @@
+package kr.teamfrankly.franklykeywordapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FranklyKeywordApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FranklyKeywordApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/teamfrankly/franklykeywordapi/FranklyKeywordApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.teamfrankly.franklykeywordapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranklyKeywordApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
